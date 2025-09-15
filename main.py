a6a2fe904b96805a185507841c28322ef6c3f9f4
shturman99 <mgurgeni@andrew.cmu.edu>
2025-09-14 22:40:10 -0400

    last commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_text(path):
     with open(path) as f:
@@ -7,9 +8,26 @@ def get_text(path):
     return contents
 
 def main():
-    contents = get_text("books/frankenstein.txt")
-    #print(contents)
-    number = len(num_words("books/frankenstein.txt"))
-    print(f"{number} words found in the document")
+    if len(sys.argv)<=1:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]               
+        contents = get_text(path)
+        #print(contents)
+        number = len(num_words(path=path))
+        print(f"{number} words found in the document")
+        dic = char_count(path=path)
+        s_dic = sorted_dic(dic)
+        #print(s_dic)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {number} total words")
+        print(f"--------- Character Count -------")
+        for line in s_dic:
+            if line['char'].isalpha():
+                print(f"{line["char"]}: {line["count"]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,28 @@ def num_words(path):
         words = f.read(-1).split()
 
     return words
+
+def char_count(path):
+    dic = {}
+    with open(path) as f:
+        for line in f.readlines():
+            for char in line.lower():
+                #print(f"Char is {char}")
+                if char in dic.keys():
+                    dic[char] +=1
+                else:
+                    dic[char] = 1
+
+    return dic
+
+def sort_on(item):
+    return item["count"]
+
+def sorted_dic(dic):
+    list = []
+    for key, val in dic.items():
+        list.append({"char":key, "count":val})
+
+    list.sort(reverse=True, key=sort_on)
+
+    return list
